3e8b3d1a8d47d1e4581752923db3eb02b634db4c
Aju100 <ajutamang10@outlook.com>
2021-04-13 10:44:23 +0545

    fix: minor changes

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@
 
 install_requirements(){
     sudo apt update -y
-    sudo apt install docker.io 
+    sudo apt install docker.io -y
     sudo systemctl start docker
     sudo systemctl enable docker
 }
