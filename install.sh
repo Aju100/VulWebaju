b4fb46e9950205de127f2675dda3076d904fd758
Aju100 <ajutamang10@outlook.com>
2021-04-13 10:47:41 +0545

    fix: docker-compose

--- install.sh
+++ install.sh
@@ -78,6 +78,7 @@ installMultidaee(){
 installNodegoat(){
     install_requirements
     sudo curl -L "https://github.com/docker/compose/releases/download/1.29.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
     git clone https://github.com/OWASP/NodeGoat.git
     cd NodeGoat
     sudo docker-compose build
